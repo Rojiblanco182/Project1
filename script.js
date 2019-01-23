a03eb7e0f25b9127744d927553b890208f598bee
Rojiblanco182 <robertosalinas1988@gmail.com>
2019-01-23 18:22:56 +0100

    function to kill enemies added

--- script.js
+++ script.js
@@ -23,35 +23,77 @@ var Game = {
       this.framesCounter++;
       this.generateObstacle();
 
-      // if (this.framesCounter > 1000) {
-      //   this.framesCounter = 0;
-      // }
+      if (this.framesCounter > 1000) {
+        this.framesCounter = 0;
+      }
 
-      // if (this.framesCounter % 50 === 0) {
-      //   this.generateObstacle();
-      // }
+      if (this.framesCounter % 150 === 0) {
+        this.generateEnemy();
+      }
 
       //this.score += 0.01;
       this.moveAll();
       this.drawAll();
 
-      this.clearObstacles();
+      //this.clearObstacles();
+      this.enemyKilled();
+
     }.bind(this), 1000 / this.fps);
   },
 
   reset: function () {
     this.background = new Background(this);
     this.player = new Player(this);
-    this.enemy = new Enemy(this);
+    this.enemies = [];
     this.framesCounter = 0;
     this.obstacles = [];
     //this.score = 0;
   },
 
-  clearObstacles: function () {
-    this.obstacles = this.obstacles.filter(function (obstacle) {
-      return obstacle.x >= 0;
-    });
+  enemyKilled: function () {
+    this.enemies.forEach(function (enemy, i) {
+      this.player.batarangs.forEach(function (batarang) {
+
+        if (
+          batarang.x <= enemy.x + enemy.w &&
+          batarang.x + batarang.w >= enemy.x
+        ) {
+          this.enemies.splice(i, 1);
+        }
+      }.bind(this))
+    }.bind(this))
+  },
+
+  checkCollision: function () {
+    // this.left = function () { return this.x };
+    // this.right = function () { return (this.x + this.w) };
+    // this.top = function () { return this.y };
+    // this.bottom = function () { return (this.y + this.h) };
+
+    // this.crashWith = function () {
+    //   return !((this.bottom() < obstacle.top()) ||
+    //     (this.left() > obstacle.right()) ||
+    //     (this.right() < obstacle.left()) ||
+    //     (this.top() > obstacle.bottom()))
+    // }
+    return this.obstacles.some(function (obstacle) {
+      return (
+        ((this.player.x + this.player.w) >= obstacle.x &&
+          (obstacle.x + obstacle.w) > this.player.x &&
+          (this.player.y + this.player.h) > obstacle.y &&
+          (obstacle.y + obstacle.h) > this.player.y)
+      );
+    }.bind(this));
+  },
+
+  // clearObstacles: function () {
+  //   this.obstacles = this.obstacles.filter(function (obstacle) {
+  //     return obstacle.x >= 0;
+  //   });
+  // },
+
+  generateEnemy: function () {
+    this.enemies.push(new Enemy(this));
   },
 
   generateObstacle: function () {
@@ -65,13 +107,13 @@ var Game = {
   drawAll: function () {
     this.background.draw();
     this.player.draw();
-    this.enemy.draw();
+    this.enemies.forEach(function (enemy) { enemy.draw(); });
     this.obstacles.forEach(function (obstacle) { obstacle.draw(); });
   },
 
   moveAll: function () {
     //this.player.move();
-    this.enemy.move();
+    this.enemies.forEach(function (enemy) { enemy.move(); });
     this.player.setListeners();
     //this.obstacles.forEach(function (obstacle) { obstacle.move(); });
   }
